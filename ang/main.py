7354964923cbb49a579b1c4cd6031557001d55c7
Lovro Terseglav <lovroterseglav@protonmail.com>
2023-10-15 20:33:05 +0200

    fix

--- ang/main.py
+++ ang/main.py
@@ -10,8 +10,8 @@ TENSES = ["Infinitive", "Past", "Past Participle"]
 
 def ask(slo: str, d: tuple[str, str, str]):
     f = lambda x: [i.strip() for i in x.split("/")]
-
-    print(f"------------------------ {slo} ------------------------\n")
+    status = []
+    print(f"------------------------ {slo} ------------------------")
 
     for tenese, verb in zip(TENSES, d):
         options = f(verb)
@@ -19,12 +19,19 @@ def ask(slo: str, d: tuple[str, str, str]):
         if resp == "exit":
             exit(0)
         if resp in options:
-            wasabi.msg.good(f"{resp} ({'/'.join(options)})")
-            print()
+            status.append((True, f"{resp} ({'/'.join(options)})"))
             continue
-        wasabi.msg.fail(f"{resp} ({'/'.join(options)})")
-        print()
+        status.append((False, f"{resp} ({'/'.join(options)})"))
 
+    print()
+
+    for s, r in status:
+        if s:
+            wasabi.msg.good(r)
+        else:
+            wasabi.msg.fail(r)
+
+    print()
 
 if __name__ == "__main__":
     with open("ang/verbs.csv") as fr:
